f528593907b705c7c55853b72a6122a89a964be0
迈萌 <8846516@qq.com>
2023-11-08 10:34:43 +0800

    Update system_config.py

--- config/system_config.py
+++ config/system_config.py
@@ -9,7 +9,7 @@
 """
 
 # ChatGPT api config
-api_chatgpt_key = ''
+api_chatgpt_key = '<YOUR_API_KEY>'
 api_chatgpt_model = "gpt-3.5-turbo-16k"
 api_chatgpt_temperature = 0.2
 api_chatgpt_max_token = 2000 # the maximum token value for a single question
@@ -17,13 +17,13 @@ api_chatgpt_max_tokens = 8000 # the maximum token value of all questions and ans
 
 # Encryption and decryption api config
 api_decrypt_url = 'http://www.cmd5.com/api.ashx'
-api_decrypt_email = ''
-api_decrypt_key = ''
+api_decrypt_email = '<YOUR_API_EMAIL>'
+api_decrypt_key = '<YOUR_API_KEY>'
 
 # Open ports query api config
 api_ports_url = 'https://fofa.info/api/v1/search/all'
-api_ports_email = ''
-api_ports_key = ''
+api_ports_email = '<YOUR_API_EMAIL>'
+api_ports_key = '<YOUR_API_KEY>'
 api_ports_fields = 'host,ip,port'
 api_ports_page = 1
 api_ports_size = 200
@@ -38,7 +38,7 @@ api_dns_url = 'https://api.hackertarget.com/dnslookup/'
 api_reputation_url_base = 'https://api.threatbook.cn/v3/'
 api_reputation_url_ip_reputation = 'scene/ip_reputation'
 api_reputation_url_file_reputation = 'file/report'
-api_reputation_key = ''
+api_reputation_key = '<YOUR_API_KEY>'
 
 # The maximum length returned by the function, to prevent the message sent to openai from being too long
 function_return_max_len = 2000
